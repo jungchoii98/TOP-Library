73c5ef21db65bde7afc91c47fde7ce21b9f05e39
Jung Choi <choi343@usc.edu>
2023-09-24 16:36:16 -0400

    Implement fundamental functionailty
    Implement HTML, basic CSS, and JS functionality

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(
+    title = 'N/A',
+    author = 'N/A',
+    pages = 'N/A',
+    read = false
+  ) {
+    let book = new Book(title, author, pages, read);
+    this.books.push(book);
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+  }
+
+  updateReadStatus(index, read) {
+    this.books[index].read = read;
+  }
+
+  getNumberOfBooks() {
+    return this.books.length;
+  }
+
+  getBookAt(index) {
+    return this.books[index];
+  }
+}
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.getReadString = () => {
+    if (this.read) { return "Read"; }
+    return "Not Read";
+  }
+}
+
+const library = new Library();
+
 let showDialogElement = document.querySelector('#show-dialog');
 let dialogElement = document.querySelector('dialog');
 let titleInputElement = document.querySelector('#title-input');
@@ -8,6 +53,7 @@ let cancelButtonElement = document.querySelector('#cancel-button');
 let addButtonElement = document.querySelector('#add-button');
 
 showDialogElement.addEventListener("click", (event) => {
+  refreshModalInputs();
   dialogElement.showModal();
 });
 
@@ -22,56 +68,63 @@ addButtonElement.addEventListener("click", (event) => {
   let author = authorInputElement.value;
   let pages = parseInt(pageInputElement.value);
   let read = readInputElement.value === "on" ? true : false;
-  addBookToLibrary(title, author, pages, read);
+  library.addBook(title, author, pages, read);
   displayBooks();
   dialogElement.close();
 });
 
-const myLibrary = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.getReadString = () => {
-    if (this.read) { return "Read"; }
-    return "Not Read";
-  }
-}
-
-function addBookToLibrary(title, author, pages, read) {
-  let book = new Book(title, author, pages, read);
-  myLibrary.push(book);
+function refreshModalInputs() {
+  titleInputElement.value = '';
+  authorInputElement.value = '';
+  pageInputElement.value = '';
+  readInputElement.value = '';
 }
 
 function displayBooks() {
   let articleElement = document.querySelector("article");
+  articleElement.innerHTML = ``;
 
-  for (let i=0; i<myLibrary.length; i++) {
-    let unorderedList = document.createElement('ul');
-    let titleListItem = document.createElement('li');
-    titleListItem.innerHTML = myLibrary[i].title;
-    let authorListItem = document.createElement('li');
-    authorListItem.innerHTML = myLibrary[i].author;
-    let pagesListItem = document.createElement('li');
-    pagesListItem.innerHTML = myLibrary[i].pages;
-    let readBtnListItem = document.createElement('li');
-    readBtnListItem.innerHTML = myLibrary[i].getReadString();
-    let removeBtnListItem = document.createElement('li');
-    removeBtnListItem.innerHTML = 'Remove';
+  for (let i=0; i<library.getNumberOfBooks(); i++) {
+    let bookCard = document.createElement('div');
+    let titleElement = document.createElement('p');
+    titleElement.innerHTML = library.getBookAt(i).title;
+    let authorElement = document.createElement('p');
+    authorElement.innerHTML = library.getBookAt(i).author;
+    let pagesElement = document.createElement('p');
+    pagesElement.innerHTML = library.getBookAt(i).pages;
+    let readBtn = document.createElement('button');
+    readBtn.innerHTML = library.getBookAt(i).getReadString();
+    readBtn.addEventListener("click", toggleRead);
+    let removeBtn = document.createElement('button');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.addEventListener("click", removeBook);
 
-    unorderedList.appendChild(titleListItem);
-    unorderedList.appendChild(authorListItem);
-    unorderedList.appendChild(pagesListItem);
-    unorderedList.appendChild(readBtnListItem);
-    unorderedList.appendChild(removeBtnListItem);
-    articleElement.appendChild(unorderedList);
+    bookCard.appendChild(titleElement);
+    bookCard.appendChild(authorElement);
+    bookCard.appendChild(pagesElement);
+    bookCard.appendChild(readBtn);
+    bookCard.appendChild(removeBtn);
+    articleElement.appendChild(bookCard);
   }
 }
 
-addBookToLibrary("The Count of Monte Cristo", "Alexandre Dumas", 1024, false);
+function toggleRead(event) {
+  let card = event.target.parentNode;
+  let index = Array.from(card.parentNode.children).indexOf(card);
+  let newReadStatus = !library.getBookAt(index).read;
+  library.updateReadStatus(index, newReadStatus);
+  displayBooks();
+}
 
-addBookToLibrary("A Short History of Nearly Everything", "Bill Bryson", 324, false);
+function removeBook(event) {
+  let card = event.target.parentNode;
+  let index = Array.from(card.parentNode.children).indexOf(card);
+  library.removeBook(index);
+  displayBooks();
+}
+
+library.addBook("The Count of Monte Cristo", "Alexandre Dumas", 1024, false);
+
+library.addBook("A Short History of Nearly Everything", "Bill Bryson", 324, false);
 
 displayBooks();
